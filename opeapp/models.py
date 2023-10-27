5e00ecfaf4d713fef61fca96735d00c3e35026ab
Teemu Ruokokoski <truoko@gmail.com>
2023-10-27 13:21:12 +0300

    Create models

--- opeapp/models.py
+++ opeapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class CourseStudent(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    grade = models.IntegerField(null=True)  # Add "null=True" to make grade optional
+
+    def __str__(self):
+        return f"{self.course.name} - {self.student.name}"
+    
+    class Meta:
+        unique_together = ('course', 'student')  # To create a composite unique constraint
+
